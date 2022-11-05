89d7f38d2c21b9c731ad64378cadb18c30b28a16
xvyaxi <xvyaxi@outlook.com>
2022-11-05 15:11:34 +0800

    mutex

---
+++ src/main/java/com/review/thread/current/aqs/Mutex.java
@@ -0,0 +1,8 @@
+/**
+ * @program java-review-demo 
+ * @description: AQS concurrent 
+ * @author: xuyaxi 
+ * @create: 2022/11/05 14:54 
+ */
+public class Mutex {
+}
---
+++ src/main/java/com/review/thread/current/aqs/TestMutex.java
@@ -0,0 +1,8 @@
+/**
+ * @program java-review-demo 
+ * @description:  
+ * @author: xuyaxi 
+ * @create: 2022/11/05 15:02 
+ */
+public class TestMutex {
+}
