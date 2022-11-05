28d7c17ddc68be6ff8e21cefd220986e0a91b705
xvyaxi <xvyaxi@outlook.com>
2022-11-05 15:22:06 +0800

    mutex

--- src/main/java/com/review/thread/current/aqs/Mutex.java
+++ src/main/java/com/review/thread/current/aqs/Mutex.java
@@ -1,8 +1,63 @@
+package com.review.thread.current.aqs;
+
+import java.io.Serializable;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
 /**
- * @program java-review-demo 
- * @description: AQS concurrent 
- * @author: xuyaxi 
- * @create: 2022/11/05 14:54 
+ * @program java-review-demo
+ * @description: AQS concurrent
+ * @author: xuyaxi
+ * @create: 2022/11/05 14:54
  */
-public class Mutex {
+public class Mutex implements Serializable {
+
+
+    /**
+     * 静态内部类，继承AQS
+     */
+    private static class Sync extends AbstractQueuedSynchronizer{
+        @Override
+        protected boolean tryAcquire(int arg) {
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            if (getState() == 0) {
+                throw new IllegalMonitorStateException();
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        // 是否处于占用状态
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+    }
+
+    private final Sync sync = new Sync();
+
+    public void lock(){
+        sync.acquire(1);
+    }
+
+    public void tryLock(){
+        sync.tryAcquire(1);
+    }
+
+    public void unlock(){
+        sync.release(1);
+    }
+
+    public boolean isLocked(){
+        return sync.isHeldExclusively();
+    }
+
 }
--- src/main/java/com/review/thread/current/aqs/TestMutex.java
+++ src/main/java/com/review/thread/current/aqs/TestMutex.java
@@ -1,8 +1,74 @@
+package com.review.thread.current.aqs;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 /**
- * @program java-review-demo 
- * @description:  
- * @author: xuyaxi 
- * @create: 2022/11/05 15:02 
+ * @program java-review-demo
+ * @description:
+ * @author: xuyaxi
+ * @create: 2022/11/05 15:02
  */
 public class TestMutex {
+
+    private static CyclicBarrier barrier = new CyclicBarrier(31);
+    private static int a = 0;
+    private static int threadCount = 30;
+    private static int addCount = 10000;
+    private static Mutex mutex = new Mutex();
+
+    public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
+        for (int i = 0; i < threadCount; i++) {
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i1 = 0; i1 < addCount; i1++) {
+                        increment1();
+                    }
+                    try {
+                        barrier.await();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    } catch (BrokenBarrierException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            t.start();
+        }
+        barrier.await();
+        System.out.println("加锁前：a=" + a);
+        barrier.reset();
+        a = 0;
+        for (int i = 0; i < threadCount; i++) {
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i1 = 0; i1 < addCount; i1++) {
+                        increment2();
+                    }
+                    try {
+                        barrier.await();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    } catch (BrokenBarrierException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            t.start();
+        }
+        barrier.await();
+        System.out.println("加锁后：a=" + a);
+    }
+
+    private static void increment2() {
+        mutex.lock();
+        a++;
+        mutex.unlock();
+    }
+
+    private static void increment1() {
+        a++;
+    }
 }
