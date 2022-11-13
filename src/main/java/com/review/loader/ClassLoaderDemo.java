93aabf445f5b0c64801309bcf34a9b220e5dc876
xvyaxi <xvyaxi@outlook.com>
2022-11-13 22:48:42 +0800

    classloader

--- src/main/java/com/review/loader/ClassLoaderDemo.java
+++ src/main/java/com/review/loader/ClassLoaderDemo.java
@@ -10,6 +10,7 @@ public class ClassLoaderDemo {
 
     public static void main(String[] args) {
         System.out.println("ClassLoaderDemo's classloader is:" + ClassLoaderDemo.class.getClassLoader());
+        System.out.println("ClassLoaderDemo's classloader name is:" + ClassLoaderDemo.class.getClassLoader().getName());
         System.out.println("The parent of ClassLoaderDemo's classloader is:" + ClassLoaderDemo.class.getClassLoader().getParent());
         System.out.println("The grandParent of ClassLoaderDemo's classloader is:" + ClassLoaderDemo.class.getClassLoader().getParent().getParent());
 
